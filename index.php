5407d8cb25faa6ec1980f5fed6a9448f4ce844dc
Kenneth Prado <Kennethprado45@gmail.com>
2015-01-07 12:29:28 -0800

    Lecture Project Some random code

--- index.php
+++ index.php
@@ -1,1 +1,112 @@
-index.php
+<?php 
+<-- EXAMPLE 1: --->
+	class Frog {
+		//body
+		//body
+		//body
+	}
+	class movies {
+		//body
+		//body
+		//body
+	}
+	class car {
+		//body
+		//body
+		//body
+	}
+
+<!-- EXAMPLE 2: -->
+	$frog1 = newfrog();
+	$frog2 = newfrog();
+
+	$movies1 = newmovies();
+	$movies2 = newmovies();
+
+	$car1 = newcar();
+	$car2 = newcar();
+
+
+<!-- EXAMPLE 3: -->
+	class frog {
+		public $animal = amphibian;
+		public $color = green;
+		public $size = "7";
+		public $height = "2'5";
+	}
+
+
+	class movies {
+		public $action = Expendables;
+		public $horror = Insidous;
+		public $comedy = The Anchor Man;
+		public $adventure = Zombie Land;
+	}
+
+	class car {
+		public $agility = 180 km/h;
+		public $brand = BMW;
+		public $color = blue;
+		public $height = "5'9";
+	}
+
+<!-- EXAMPLE 4: -->
+
+	$frog1 = new frog();
+	print $frog1>ribbet;
+	// default name
+	
+
+	$movies1 = new movies();
+	print $movies1>Insidous;
+	// default name
+
+	$car1 = new car();
+	print $car1>volvo;
+	// default name
+
+<!-- EXAMPLE 5: -->
+
+	public function myMethod( $argument, $another) {
+	// stuff
+	}
+
+	public function myMethod( $argument, $another) {
+	// stuff
+	}
+
+	public function myMethod( $argument, $another) {
+	// stuff
+	}
+
+<!-- EXAMPLE 6: -->
+	
+	class frog {
+		public $animal = amphibian;
+		public $color = green;
+		public $size = "7";
+		public $height = "2'5";
+
+		function getAnimal() {
+		return "{$this->animal}";
+		“{$this->color}”;
+		}
+	}
+	$frog1 = new Frog();
+	$frog1>animal = “amphibian”;
+	$frog1>color = “green”;
+
+	print “The frog’s animal is {$frog1>getAnimal()}.”;
+
+	//The frog's animal is an amphibian.
+
+
+
+
+
+
+
+
+
+
+ ?>
